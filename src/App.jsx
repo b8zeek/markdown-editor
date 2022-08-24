5a2d68f2f238137d0358ebd9cd6ac1d3df5268e0
Basic Mirko <bejzik8@gmail.com>
2022-08-24 02:13:53 +0100

    Created Paragraph and Button components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import styled from 'styled-components'
 
 function App() {
-    return <Container>Mirko Basic</Container>
+    return (
+        <Container>
+            <Paragraph>Mirko Basic</Paragraph>
+            <Button>Get GitHub User</Button>
+        </Container>
+    )
 }
 
 const Container = styled.div`
     width: 100%;
+    max-width: 1280px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 20px 0;
+    margin: 0 auto;
+`
+
+const Paragraph = styled.p`
+    text-align: center;
+    margin: 0;
+`
+
+const Button = styled.a`
+    display: inline-block;
+    vertical-align: top;
+    padding: 4px 20px;
+    border-radius: 5px;
+    user-select: none;
+    background-color: rgba(255, 255, 255, 0.3);
+    cursor: pointer;
+    align-self: center;
+
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.4);
+    }
 `
 
 export default App
