89558601fa8ed39d5ac29ca3d3655527af29b7dc
Basic Mirko <bejzik8@gmail.com>
2022-08-24 01:53:28 +0100

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+function App() {
+
+  return (
+    <Container>
+      Mirko Basic
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%;
+`
+
+export default App
