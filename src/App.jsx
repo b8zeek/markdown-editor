31d1415f19950af6de31f68cdc3282b77adf3ea0
Basic Mirko <bejzik8@gmail.com>
2022-08-24 17:03:57 +0100

    Updated application layout.

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,33 @@ import HomePage from './pages/HomePage'
 function App() {
     return (
         <Container>
-            <HomePage />
+            <Header />
+            <Content>
+                <HomePage />
+            </Content>
         </Container>
     )
 }
 
 const Container = styled.div`
     width: 100%;
+`
+
+const Header = styled.div`
+    width: 100%;
+    height: 48px;
+    position: fixed;
+    background-color: rgba(255, 255, 255, 0.1);
+    top: 0;
+    margin: 0 auto;
+`
+
+const Content = styled.div`
+    width: 100%;
     max-width: 1280px;
     display: flex;
     flex-direction: column;
-    padding: 20px 0;
+    padding: 64px 0 20px;
     margin: 0 auto;
 `
 
