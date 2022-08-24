dffa631e9af565ef017fecf4070cd9d26c66b476
Basic Mirko <bejzik8@gmail.com>
2022-08-25 00:12:55 +0100

    Apollo client instance provided to the application.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './graphql'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
