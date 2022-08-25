685a5b753001b1615b1a16f4b3a6076a8d6d6010
Basic Mirko <bejzik8@gmail.com>
2022-08-25 09:21:37 +0100

    Updated GET_USER_AND_REPOSITORIES query.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const GET_USER = gql`
-    query getUser {
+    query GetUser {
         viewer {
             avatarUrl
             bio
@@ -9,6 +9,37 @@ export const GET_USER = gql`
             login
             name
             url
+        }
+    }
+`
+
+export const GET_USER_AND_REPOSITORIES = gql`
+    query GetUserAndRepositories {
+        viewer {
+            avatarUrl
+            bio
+            email
+            login
+            name
+            url
+            repositories(first: 100, orderBy: { field: UPDATED_AT, direction: DESC }) {
+                totalCount
+                pageInfo {
+                    startCursor
+                    endCursor
+                    hasPreviousPage
+                    hasNextPage
+                }
+                nodes {
+                    id
+                    createdAt
+                    description
+                    name
+                    updatedAt
+                    url
+                    visibility
+                }
+            }
         }
     }
 `
