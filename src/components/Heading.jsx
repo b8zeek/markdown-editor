80cc9d0253c005dce2459a672f52ecde7954aa14
Basic Mirko <bejzik8@gmail.com>
2022-08-25 09:20:52 +0100

    Created Heading component, updated Pre component, created components mutual export.

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Heading = ({ children }) => <StyledH1>{children}</StyledH1>
+
+const StyledH1 = styled.h1`
+    line-height: 32px;
+    font-size: 24px;
+    margin: 0;
+`
--- src/components/Pre.jsx
+++ src/components/Pre.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 
-const Pre = ({ children }) => <StyledPre>{children}</StyledPre>
+export const Pre = ({ children }) => <StyledPre>{children}</StyledPre>
 
 const StyledPre = styled.pre`
     line-height: 16px;
     font-size: 12px;
     font-family: JetBrainsMono;
 `
-
-export default Pre
