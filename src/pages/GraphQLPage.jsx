fb354530e79a2a8629a81efc1587150866f39bee
Basic Mirko <bejzik8@gmail.com>
2022-08-26 02:22:38 +0100

    Added user info section to GraphQLPage component.

--- src/pages/GraphQLPage.jsx
+++ src/pages/GraphQLPage.jsx
@@ -12,7 +12,20 @@ const GraphQLPage = () => {
 
     return (
         <Container>
-            <h1>GraphQL Page</h1>
+            <UserInfoContainer>
+                <PageName>GraphQL Page</PageName>
+                {repositoriesData?.viewer && (
+                    <UserData>
+                        <UserInfo>
+                            <UserName bold marginBottom>
+                                {repositoriesData.viewer.name}
+                            </UserName>
+                            <UserName>{repositoriesData.viewer.bio}</UserName>
+                        </UserInfo>
+                        <UserImage src={repositoriesData.viewer.avatarUrl} />
+                    </UserData>
+                )}
+            </UserInfoContainer>
             {repositoriesData && (
                 <>
                     <Heading>User Data</Heading>
@@ -27,6 +40,43 @@ const Container = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
+`
+
+const UserInfoContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 40px;
+`
+
+const PageName = styled.h1`
+    display: inline-block;
+    margin: 0;
+`
+
+const UserData = styled.div`
+    padding: 10px;
+    border: 1px solid #d0d7de;
+    border-radius: 5px;
+`
+
+const UserInfo = styled.div`
+    display: inline-block;
+    vertical-align: top;
+    margin-right: 20px;
+`
+
+const UserName = styled.p`
+    line-height: 16px;
+    margin: 0;
+
+    ${({ bold }) => bold && 'font-weight: 800;'}
+    ${({ marginBottom }) => marginBottom && 'margin-bottom: 8px;'}
+`
+
+const UserImage = styled.img`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
 `
 
 export default GraphQLPage
