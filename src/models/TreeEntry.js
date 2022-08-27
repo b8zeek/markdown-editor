fbc024e2ed323792a0a21082532d030f1e8479a6
Basic Mirko <bejzik8@gmail.com>
2022-08-27 04:34:43 +0100

    Updated TreeEntry class.

--- src/models/TreeEntry.js
+++ src/models/TreeEntry.js
@@ -1,5 +1,6 @@
 export class TreeEntry {
     constructor(data) {
+        this.id = data.oid || ''
         this.name = data.name || ''
         this.path = data.path || ''
         this.size = data.size || 0
