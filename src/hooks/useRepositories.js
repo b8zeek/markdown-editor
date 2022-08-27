883d23bd28a0e44c5d6b636c3690e636929d4d26
Basic Mirko <bejzik8@gmail.com>
2022-08-27 02:17:28 +0100

    Updated useRepositories hook, now it returns parsed data.

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client/react'
 import { GET_USER_AND_REPOSITORIES } from '../graphql'
 
+import { parseUserAndRepositoriesData } from '../utils'
+
 export function useRepositories() {
     const { data } = useQuery(GET_USER_AND_REPOSITORIES)
 
-    return data
+    return parseUserAndRepositoriesData(data)
 }
