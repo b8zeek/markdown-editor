71e9a31e36b7beea0e9dcb4c7f15dc00cd40a063
Basic Mirko <bejzik8@gmail.com>
2022-08-27 02:38:47 +0100

    Updated User class.

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,11 @@
 export class User {
     constructor(data) {
-        this.login = data.login
-        this.email = data.email
-        this.url = data.url
-        this.name = data.name
-        this.bio = data.bio
-        this.avatarUrl = data.avatarUrl
+        console.log('PARS DATA', data)
+        this.login = data?.login || ''
+        this.email = data?.email || ''
+        this.url = data?.url || ''
+        this.name = data?.name || ''
+        this.bio = data?.bio || ''
+        this.avatarUrl = data?.avatarUrl || ''
     }
 }
