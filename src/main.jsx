30b90180a76add9ab86032527931ae28f1f6e835
Basic Mirko <bejzik8@gmail.com>
2022-08-28 00:00:26 +0100

    App wrapped with AuthenticationGuard component.

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import './index.css'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './apollo'
 
+import { AuthenticationGuard } from './guards'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <ApolloProvider client={client}>
-                <App />
+                <AuthenticationGuard>
+                    <App />
+                </AuthenticationGuard>
             </ApolloProvider>
         </BrowserRouter>
     </React.StrictMode>
