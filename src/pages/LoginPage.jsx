197f1abb46896eb358da68b13790895f35fbf310
Basic Mirko <bejzik8@gmail.com>
2022-08-28 00:31:55 +0100

    Created Button component.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ export function LoginPage() {
     return (
         <Container>
             <Input />
+            <Button>Authenticate</Button>
         </Container>
     )
 }
@@ -14,6 +15,7 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     margin: 0 auto;
 `
 
@@ -26,9 +28,27 @@ const Input = styled.input`
     background-color: #0d1117;
     border: 1px solid #30363d;
     border-radius: 6px;
+    margin-bottom: 10px;
 
     &:focus {
         background-color: #161b22;
         outline: none;
+    }
+`
+
+const Button = styled.button`
+    line-height: 20px;
+    font-size: 12px;
+    font-weight: 500;
+    color: #c9d1d9;
+    background-color: #21262d;
+    border: 1px solid rgba(240, 246, 252, 0.1);
+    border-radius: 6px;
+    padding: 3px 12px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #30363d;
+        border-collapse: #8b949e;
     }
 `
