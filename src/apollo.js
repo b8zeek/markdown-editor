1541b4ea70ccb6b07c70095009168930e8fa692b
Basic Mirko <bejzik8@gmail.com>
2022-08-28 16:05:08 +0100

    Apollo client now takes access token from local storage.

--- src/apollo.js
+++ src/apollo.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
     uri: 'https://api.github.com/graphql',
     headers: {
-        Authorization: `bearer ${import.meta.env.VITE_PERSONAL_ACCESS_TOKEN}`
+        Authorization: `bearer ${localStorage.getItem('personal-access-token')}`
     },
     cache: new InMemoryCache()
 })
