2f12d50662fca69096e5bb4006e4bdb4bf962b21
Basic Mirko <bejzik8@gmail.com>
2022-08-28 16:06:29 +0100

    Updated AuthenticationGuard, now it checks for the token in storage and handles token adding.

--- src/guards/AuthenticationGuard.jsx
+++ src/guards/AuthenticationGuard.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
+import { useApolloClient } from '@apollo/client'
+
 import { LoginPage } from '../pages'
 
 export function AuthenticationGuard({ children }) {
-    const personalAccessToken = localStorage.getItem('personal-access-token')
+    const [personalAccessToken, setPersonalAccessToken] = useState(null)
+    const client = useApolloClient()
 
-    return personalAccessToken ? children : <LoginPage />
+    useEffect(() => {
+        const storageToken = localStorage.getItem('personal-access-token')
+        setPersonalAccessToken(!!storageToken)
+    }, [])
+
+    const addNewToken = token => {
+        console.log(client)
+        console.log(token)
+        client.link.options.headers.Authorization = `bearer ${token}`
+        setPersonalAccessToken(token)
+        localStorage.setItem('personal-access-token', token)
+    }
+
+    return personalAccessToken ? children : <LoginPage addNewToken={addNewToken} />
 }
