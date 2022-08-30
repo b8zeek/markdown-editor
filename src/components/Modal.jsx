a994ee389110e756dad455085a9a888c9321f7f7
Basic Mirko <bejzik8@gmail.com>
2022-08-30 21:32:11 +0100

    Updated Modal component, created Textarea component.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 import { useState, useEffect } from 'react'
 
-export function Modal({ children }) {
+export function Modal() {
     const [modalContainer] = useState(() => document.createElement('div'))
 
     useEffect(() => {
@@ -15,5 +16,31 @@ export function Modal({ children }) {
         }
     }, [])
 
-    return ReactDOM.createPortal(children, modalContainer)
+    return ReactDOM.createPortal(
+        <MarkdownModal>
+            <Textarea />
+        </MarkdownModal>,
+        modalContainer
+    )
 }
+
+const MarkdownModal = styled.div`
+    min-height: 500px;
+    height: 60vh;
+    width: 100%;
+    max-width: 768px;
+    border-radius: 6px;
+    background-color: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(5px);
+    padding: 20px;
+`
+
+const Textarea = styled.textarea`
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    border: none;
+    background-color: transparent;
+    outline: none;
+    resize: none;
+`
