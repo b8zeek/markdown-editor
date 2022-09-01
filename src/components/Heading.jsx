f4086730799fa79d8517087dbaa9f4caaed733a1
Basic Mirko <bejzik8@gmail.com>
2022-09-01 02:14:41 +0100

    Updated Heading component.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,5 +5,5 @@ export const Heading = ({ children }) => <StyledH1>{children}</StyledH1>
 const StyledH1 = styled.h1`
     line-height: 32px;
     font-size: 24px;
-    margin: 0;
+    margin: 0 0 24px;
 `
