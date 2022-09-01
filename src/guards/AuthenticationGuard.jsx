dbba053c102320ee4ef3fea9c8d17694b1595dfc
Basic Mirko <bejzik8@gmail.com>
2022-09-01 01:12:34 +0100

    Updated AuthenticationGuard's logic.

--- src/guards/AuthenticationGuard.jsx
+++ src/guards/AuthenticationGuard.jsx
@@ -3,17 +3,23 @@ import { useApolloClient } from '@apollo/client'
 
 import { personalAccessTokenAtom } from '../state'
 
+import { useUser } from '../hooks'
+
 import { LoginPage } from '../pages'
 
 export function AuthenticationGuard({ children }) {
     const [personalAccessToken, setPersonalAccessToken] = useAtom(personalAccessTokenAtom)
     const client = useApolloClient()
 
+    const { data, refetch } = useUser(personalAccessToken)
+
     const addNewToken = token => {
         client.link.options.headers.Authorization = `bearer ${token}`
         setPersonalAccessToken(token)
         localStorage.setItem('personal-access-token', token)
+
+        refetch()
     }
 
-    return personalAccessToken ? children : <LoginPage addNewToken={addNewToken} />
+    return data.login ? children : <LoginPage addNewToken={addNewToken} />
 }
