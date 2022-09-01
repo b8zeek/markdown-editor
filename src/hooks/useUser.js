49aca978a4bef73896df6ec55544416293845e3e
Basic Mirko <bejzik8@gmail.com>
2022-09-01 01:21:15 +0100

    Updated useUser hook.

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,12 +1,16 @@
 import { useQuery } from '@apollo/client/react'
 import { GET_USER } from '../graphql'
+import { useAtomValue } from 'jotai'
+import { personalAccessTokenAtom } from '../state'
 
 import { parseUserData } from '../utils'
 
-export function useUser(personalAccessToken) {
-    const { data, refetch } = useQuery(GET_USER, {
+export function useUser() {
+    const personalAccessToken = useAtomValue(personalAccessTokenAtom)
+
+    const { data, loading, refetch } = useQuery(GET_USER, {
         skip: !personalAccessToken
     })
 
-    return { data: parseUserData(data), refetch }
+    return { data: parseUserData(data), loading, refetch }
 }
