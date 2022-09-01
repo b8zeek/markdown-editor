841bec7e7d8de28d53aeea92e59f08c2fa55f883
Basic Mirko <bejzik8@gmail.com>
2022-09-01 22:23:39 +0200

    Created Button component.

---
+++ src/components/Button.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Button = ({ children }) => <StyledButton>{children}</StyledButton>
+
+const StyledButton = styled.button`
+    line-height: 20px;
+    font-size: 12px;
+    font-weight: 500;
+    color: #c9d1d9;
+    background-color: #21262d;
+    border: 1px solid rgba(240, 246, 252, 0.1);
+    border-radius: 6px;
+    padding: 3px 12px;
+    cursor: pointer;
+
+    &:hover:enabled {
+        background-color: #30363d;
+        border-collapse: #8b949e;
+    }
+
+    &:disabled {
+        color: #8b949e;
+        border: 1px solid #21262d;
+        cursor: not-allowed;
+    }
+`
