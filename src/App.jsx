83751b3b96c642fd569191c044f5e59135198112
Basic Mirko <bejzik8@gmail.com>
2022-09-01 01:41:23 +0100

    Updated App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import styled from 'styled-components'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
+import { Header } from './components'
 import { HomePage, GraphQLPage } from './pages'
 
 function App() {
     return (
         <Container>
-            <Header>
-                <HeaderContent>
-                    <StyledLink to=''>Home</StyledLink>
-                    <StyledLink to='graphql'>Graph QL</StyledLink>
-                </HeaderContent>
-            </Header>
+            <Header />
             <Content>
                 <Routes>
                     <Route path='' element={<HomePage />} />
@@ -25,41 +21,6 @@ function App() {
 const Container = styled.div`
     width: 100%;
     overflow: hidden;
-`
-
-const Header = styled.div`
-    width: 100%;
-    height: 24px;
-    position: fixed;
-    top: 0;
-    padding: 12px 0;
-    background-color: rgba(255, 255, 255, 0.1);
-    backdrop-filter: blur(5px);
-    margin: 0 auto;
-`
-
-const HeaderContent = styled.nav`
-    max-width: var(--application-max-width);
-    margin: 0 auto;
-`
-
-const StyledLink = styled(Link)`
-    display: inline-block;
-    vertical-align: top;
-    line-height: 24px;
-    padding: 0 20px;
-    font-size: 16px;
-    font-weight: 800;
-    color: white;
-    background-color: rgba(0, 0, 0, 0.4);
-    border-radius: 5px;
-    cursor: pointer;
-    margin-right: 10px;
-    text-decoration: none;
-
-    &:hover {
-        background-color: rgba(0, 0, 0, 0.2);
-    }
 `
 
 const Content = styled.div`
