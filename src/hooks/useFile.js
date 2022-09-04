abbd9c84b7461e5331ce7c58f6ae3164393d63f7
Basic Mirko <bejzik8@gmail.com>
2022-09-04 05:03:30 +0200

    Updated useRepositoryTree and useFile hooks.

--- src/hooks/useFile.js
+++ src/hooks/useFile.js
@@ -1,16 +1,21 @@
+import { useApolloClient } from '@apollo/client'
 import { useQuery } from '@apollo/client/react/hooks'
 
-import { GET_FILE } from '../graphql'
+import { GET_USER, GET_FILE } from '../graphql'
 import { parseFileData } from '../utils'
 
-export function useFile(owner, name, path) {
+export function useFile(name, path) {
+    const client = useApolloClient()
+
+    const { user } = client.readQuery({ query: GET_USER })
+
     const { data } = useQuery(GET_FILE, {
         variables: {
-            owner,
+            owner: user.login,
             name,
             path
         },
-        skip: !owner || !name || !path
+        skip: !name || !path
     })
 
     return parseFileData(data)
--- src/hooks/useRepositoryTree.js
+++ src/hooks/useRepositoryTree.js
@@ -1,16 +1,21 @@
+import { useApolloClient } from '@apollo/client'
 import { useQuery } from '@apollo/client/react/hooks'
-import { GET_REPOSITORY_TREE } from '../graphql'
 
+import { GET_USER, GET_REPOSITORY_TREE } from '../graphql'
 import { parseTreeData } from '../utils'
 
-export function useRepositoryTree(owner, name, path) {
+export function useRepositoryTree(name, path) {
+    const client = useApolloClient()
+
+    const { user } = client.readQuery({ query: GET_USER })
+
     const { data, previousData } = useQuery(GET_REPOSITORY_TREE, {
         variables: {
-            owner,
+            owner: user.login,
             name,
             path
         },
-        skip: !owner || !name || !path
+        skip: !name || !path
     })
 
     if (data) return parseTreeData(data)
