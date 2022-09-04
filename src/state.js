6a13803173d673cf62a37e2ac9072233ecdc5292
Milos Rujevic <milosrujevic@gmail.com>
2022-09-05 01:20:15 +0200

    Cleanup, add title

--- src/state.js
+++ src/state.js
@@ -1,14 +1,6 @@
 import { atomWithStorage } from 'jotai/utils'
-import { atomWithStore } from 'jotai/zustand'
-import create from 'zustand/vanilla'
 
 export const personalAccessTokenAtom = atomWithStorage(
     'personal-access-token',
     localStorage.getItem('personal-access-token')
 )
-
-// const store = create({
-//     selectedRepo: null
-// })
-
-// const storeAtom = atomWithStore(store)
