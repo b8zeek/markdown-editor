3af2f529e4e76645b79ad0b1f78d8078abfb596f
Basic Mirko <bejzik8@gmail.com>
2022-09-06 01:23:25 +0200

    Created repoStoreAtom which uses Jotai with Zustand.

--- src/state.js
+++ src/state.js
@@ -1,6 +1,21 @@
 import { atomWithStorage } from 'jotai/utils'
+import create from 'zustand/vanilla'
+import { atomWithStore } from 'jotai/zustand'
 
 export const personalAccessTokenAtom = atomWithStorage(
     'personal-access-token',
     localStorage.getItem('personal-access-token')
 )
+
+const repoStore = create(set => ({
+    repositoryName: '',
+    setRepositoryName: v => set({ repositoryName: v }),
+    branchName: '',
+    setBranchName: v => set({ branchName: v }),
+    currentPath: [],
+    setCurrentPath: v => set({ currentPath: v }),
+    selectedFile: '',
+    setSelectedFile: v => set({ selectedFile: v })
+}))
+
+export const repoStoreAtom = atomWithStore(repoStore)
