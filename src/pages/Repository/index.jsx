4fb01120694d2849a2d4b2270f662ce3f6a19a9c
Basic Mirko <bejzik8@gmail.com>
2022-09-05 23:50:23 +0200

    Updated Repository component.

---
+++ src/pages/Repository/index.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+
+import { useRepositoryTree, useFile } from '../../hooks'
+
+import { Heading, Modal } from '../../components'
+import { FilesTable } from './FilesTable'
+
+export function Repository() {
+    const [repositoryName, setRepositoryName] = useState('')
+    const [branchName, setBranchName] = useState('')
+    const [currentPath, setCurrentPath] = useState([])
+    const [selectedFile, setSelectedFile] = useState('')
+
+    const params = useParams()
+
+    useEffect(() => {
+        const [name, path] = params.repository.split('~')
+        console.log(path)
+
+        setRepositoryName(name)
+        setBranchName(path)
+    }, [])
+
+    const closeFile = () => setSelectedFile('')
+
+    const { repositoryTree, oid } = useRepositoryTree(repositoryName, `${branchName}:${currentPath.join('/')}`)
+    const fileContent = useFile(repositoryName, `${branchName}:${selectedFile}`)
+
+    const toTheRootFolder = () => setCurrentPath([])
+    const folderUp = () => setCurrentPath(currentPath => [...currentPath].splice(0, currentPath.length - 1))
+    const toFolder = index => setCurrentPath(currentPath => [...currentPath].splice(0, index + 1))
+    const openFileOrFolder = (type, path) => (type === 'tree' ? setCurrentPath(path.split('/')) : setSelectedFile(path))
+
+    return (
+        <Container>
+            {repositoryName && <Heading>{repositoryName}</Heading>}
+            {repositoryTree.length !== 0 && (
+                <FilesTable
+                    toTheRootFolder={toTheRootFolder}
+                    currentPath={currentPath}
+                    branchName={branchName}
+                    folderUp={folderUp}
+                    toFolder={toFolder}
+                    repositoryTree={repositoryTree}
+                    openFileOrFolder={openFileOrFolder}
+                />
+            )}
+            {fileContent?.text && <Modal text={fileContent.text} closeFile={closeFile} />}
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
