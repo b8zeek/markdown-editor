ea297dadc5a9546057ade8b089b46bd53697612c
Basic Mirko <bejzik8@gmail.com>
2022-09-06 01:25:44 +0200

    Created useRepoService.

---
+++ src/services/useRepoService.js
@@ -0,0 +1,20 @@
+import { useAtomValue } from 'jotai'
+import { repoStoreAtom } from '../state'
+
+export const useRepoService = () => {
+    const { setCurrentPath, setSelectedFile } = useAtomValue(repoStoreAtom)
+
+    const openFileOrFolder = (type, path) => (type === 'tree' ? setCurrentPath(path.split('/')) : setSelectedFile(path))
+    const toFolder = index => setCurrentPath(currentPath => [...currentPath].splice(0, index + 1))
+    const folderUp = () => setCurrentPath(currentPath => [...currentPath].splice(0, currentPath.length - 1))
+    const toTheRootFolder = () => setCurrentPath([])
+    const closeFile = () => setSelectedFile('')
+
+    return {
+        openFileOrFolder,
+        toFolder,
+        folderUp,
+        toTheRootFolder,
+        closeFile
+    }
+}
