335ebc03931fcca0f4d56db3647221fad2a29328
Basic Mirko <bejzik8@gmail.com>
2022-09-06 15:45:59 +0200

    Created useCommit hook.

---
+++ src/hooks/useCommit.js
@@ -0,0 +1,43 @@
+import { useApolloClient } from '@apollo/client'
+import { useMutation } from '@apollo/client'
+import { useAtomValue } from 'jotai'
+
+import { repoStoreAtom } from '../state'
+
+import { GET_USER, GET_REPOSITORY_TREE, COMMIT } from '../graphql'
+
+export const useCommit = (text, commitMessage, oid) => {
+    const client = useApolloClient()
+
+    const { repositoryName, branchName, currentPath, selectedFile } = useAtomValue(repoStoreAtom)
+
+    const { user } = client.readQuery({ query: GET_USER })
+
+    // GET REPO OID
+
+    const commitChanges = useMutation(COMMIT)[0]
+
+    console.log(currentPath)
+
+    const commitHandler = async () => {
+        console.log('COMMIT HANDLER EXECUTED...')
+        try {
+            const result = await commitChanges({
+                variables: {
+                    ownerAndRepo: `${user.login}/${repositoryName}`,
+                    branchName,
+                    message: commitMessage,
+                    path: 'test.md',
+                    content: btoa(text),
+                    oid
+                }
+            })
+
+            console.log('COMMIT DATA:', result)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return commitHandler
+}
