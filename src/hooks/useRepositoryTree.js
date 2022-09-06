3b7f712e28bb9bfdf61cc82d78d68074794a253e
Basic Mirko <bejzik8@gmail.com>
2022-09-06 23:33:52 +0200

    Updated useRepositoryTree hook to present spinner based on fetching state.

--- src/hooks/useRepositoryTree.js
+++ src/hooks/useRepositoryTree.js
@@ -1,15 +1,20 @@
+import { useEffect } from 'react'
 import { useApolloClient } from '@apollo/client'
 import { useQuery } from '@apollo/client/react/hooks'
 
 import { GET_USER, GET_REPOSITORY_TREE } from '@graphql'
 import { parseTreeData } from '@utils'
 
+import { useUIService } from '@services'
+
 export function useRepositoryTree(name, path) {
+    const { showSpinner, hideSpinner } = useUIService()
+
     const client = useApolloClient()
 
     const { user } = client.readQuery({ query: GET_USER })
 
-    const { data, previousData } = useQuery(GET_REPOSITORY_TREE, {
+    const { data, previousData, loading } = useQuery(GET_REPOSITORY_TREE, {
         variables: {
             owner: user.login,
             name,
@@ -17,6 +22,10 @@ export function useRepositoryTree(name, path) {
         },
         skip: !name || !path
     })
+
+    useEffect(() => {
+        loading ? showSpinner() : hideSpinner()
+    }, [loading])
 
     if (data) return parseTreeData(data)
     if (previousData) return parseTreeData(previousData)
