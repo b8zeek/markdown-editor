42f5f8d614a0bd369c5d2fcd8c8219dac29c9ecd
Basic Mirko <bejzik8@gmail.com>
2022-09-06 21:01:55 +0200

    Added absolute imports support.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
+const folders = ['', 'assets', 'components', 'graphql', 'guards', 'hooks', 'models', 'pages', 'services', 'utils']
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: folders.map(folder => ({ find: `@${folder}`, replacement: path.resolve(__dirname, `src/${folder}`) }))
+    },
     plugins: [react()]
 })
