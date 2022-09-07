fe6fd6e8f9a61f8666c094cd0100ad38fb377633
Basic Mirko <bejzik8@gmail.com>
2022-09-07 02:32:38 +0200

    Updated useRepositories hook.

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -4,16 +4,11 @@ import { useQuery } from '@apollo/client/react'
 import { GET_REPOSITORIES } from '@graphql'
 import { parseRepositoriesData } from '@utils'
 
-import { useUIService } from '@services'
-
 export function useRepositories() {
-    const { showSpinner, hideSpinner } = useUIService()
-
     const { data, loading } = useQuery(GET_REPOSITORIES)
 
-    useEffect(() => {
-        loading ? showSpinner() : hideSpinner()
-    }, [loading])
-
-    return parseRepositoriesData(data)
+    return {
+        data: parseRepositoriesData(data),
+        loading
+    }
 }
