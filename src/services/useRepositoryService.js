2c31747dee9e1604b6ceac1826a598cb3698fea6
Basic Mirko <bejzik8@gmail.com>
2022-09-07 14:16:02 +0200

    Fixed broken functions in useRepositoryService.

--- src/services/useRepositoryService.js
+++ src/services/useRepositoryService.js
@@ -2,11 +2,11 @@ import { useAtomValue } from 'jotai'
 import { repoStoreAtom } from '@/state'
 
 export function useRepositoryService() {
-    const { setCurrentPath, setSelectedFile } = useAtomValue(repoStoreAtom)
+    const { currentPath, setCurrentPath, setSelectedFile } = useAtomValue(repoStoreAtom)
 
     const openFileOrFolder = (type, path) => (type === 'tree' ? setCurrentPath(path.split('/')) : setSelectedFile(path))
-    const toFolder = index => setCurrentPath(currentPath => [...currentPath].splice(0, index + 1))
-    const folderUp = () => setCurrentPath(currentPath => [...currentPath].splice(0, currentPath.length - 1))
+    const toFolder = index => setCurrentPath([...currentPath].splice(0, index + 1))
+    const folderUp = () => setCurrentPath([...currentPath].splice(0, currentPath.length - 1))
     const toTheRootFolder = () => setCurrentPath([])
     const closeFile = () => setSelectedFile('')
 
