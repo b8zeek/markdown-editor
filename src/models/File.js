c921f1c7e063b0d0b42d880572cd40a7c91cf813
Basic Mirko <bejzik8@gmail.com>
2022-09-08 14:52:38 +0200

    Updated File class.

--- src/models/File.js
+++ src/models/File.js
@@ -1,5 +1,6 @@
 export class File {
     constructor(data) {
-        this.text = data.text
+        this.text = data.object.text
+        this.oid = data.defaultBranchRef.target.history.nodes[0].oid
     }
 }
